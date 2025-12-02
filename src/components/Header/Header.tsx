58bd96b79956c37ddf8df056612f8bba112f0a9e
sokerling <kotofeli@mail.ru>
2025-12-03 01:40:50 +0600

    added: added search bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,17 +3,37 @@ import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import Logo from '../../assets/Logo.svg';
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  searchTerm?: string;
+  setSearchTerm?: (value: string) => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ searchTerm, setSearchTerm }) => {
   return (
     <header className={styles.header}>
-      <div>
-        <Link to="/"> <img src={Logo} alt="Кьюрио" className={styles.logo}/></Link>
+      <div className={styles.left}>
+        <Link to="/">
+          <img src={Logo} alt="Кьюрио" className={styles.logo} />
+        </Link>
       </div>
-      <nav className={styles.nav}>
-        <Link to="/games" className={styles.link}>Игры</Link>
-        <Link to="/categories" className={styles.link}>Категории</Link>
-        <Link to="/about" className={styles.link}>О нас</Link>
-      </nav>
+
+      <div className={styles.center}>
+        {setSearchTerm && (
+          <input
+            type="text"
+            placeholder="Поиск игр..."
+            className={styles['search-input']}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        )}
+        <nav className={styles.nav}>
+          <Link to="/games" className={styles.link}>Игры</Link>
+          <Link to="/categories" className={styles.link}>Категории</Link>
+          <Link to="/about" className={styles.link}>О нас</Link>
+        </nav>
+      </div>
+
       <div className={styles.user}>
         <div className={styles.avatar}>👦</div>
       </div>
